4c337a674a97d4a64df0a3bdc6076ec4005f365e
Matthew Hammer <matthew.hammer@colorado.edu>
2016-10-14 10:09:33 -0600

    developed Homework \# 3; posting now...

--- courses/csci5535-f16/lec/ocaml1_Lang_E.ml
+++ courses/csci5535-f16/lec/ocaml1_Lang_E.ml
@@ -17,6 +17,12 @@ type exp = (* expressions in E *)
   | Cat of exp * exp
   | Let of exp * var * exp
   | Var of var
+	     
+  | Zero
+  | Succ of exp
+  | Rec of exp (* Z *) * var * var * exp (* S *) * exp (* number *)
+  | Lam of var * (* typ * *) exp
+  | App of exp * exp
 
 type typ = 
   Num | Str
@@ -61,16 +67,41 @@ let rec exp_typ : gamma -> exp -> (typ option) =
 
 
     
-let is_val : exp -> bool =
+let rec is_val : exp -> bool =
   fun e -> match e with 
 	   (* Pattern -> Arm/Body/RHS *)
 	   | Num _ -> true
 	   | Str _ -> true
+	   | Zero  -> true
+	   | Succ e -> is_val e
+(*	   | Pair (e1, e2) -> is_val e1 && is_val e2
+	   | InL e -> is_val e
+	   | InR e -> is_val e *)
 	   | _     -> false
 
 let rec subst : exp -> var -> exp -> exp =
   fun e1 x e2 ->
   match e2 with
+  | App(e21,e22) -> App((subst e1 x e21),(subst e1 x e22))
+  | Lam(y, e) -> 
+     if x <> y then Lam(y, subst e1 x e)
+     else Lam(y, e)
+
+  | Zero -> Zero
+  | Succ e -> Succ (subst e1 x e)
+  | Rec(e0, y1,y2, es, e) ->
+     assert (y1 <> y2) ;
+     if x <> y1 && (not (x = y2)) then
+       Rec(subst e1 x e0,
+	   y1, y2,
+	   subst e1 x es,
+	   subst e1 x e)
+     else
+       Rec(subst e1 x e0,
+	   y1, y2,
+	   es,
+	   subst e1 x e)
+
   | Num n -> Num n
   | Str s -> Str s
   | Var y -> 
@@ -95,6 +126,21 @@ let rec step : exp -> exp =
 	   | Var _ -> failwith "expression is not closed"
 	   | Num _ -> failwith "value does not step"
 	   | Str _ -> failwith "value does not step"
+
+	   | Rec(e0, x,y,es, e) ->
+	      if is_val e then (
+		match e with 
+		| Zero -> e0
+		| Succ(e') (* this is e *) -> 
+		   let r = Rec(e0, x,y,es, e') in
+		   let es = subst e' x es in
+		   let es = subst r  y es in
+		   es
+	      )
+	      else (
+		let e' = step e in
+		Rec(e0, x,y,es, e')
+	      )
 
 	   | Plus (e1,e2) -> 
 	      if is_val e1 then 
